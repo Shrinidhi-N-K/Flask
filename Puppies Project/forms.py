5e071da660fabab098ed7eb34d13a09ee2d308e0
shrinidhi <snkulkarni2003@gmail.com>
2025-04-28 21:11:46 +0530

    Puppies Project to connect databse(Model) with forms(views).

---
+++ Puppies Project/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import (StringField,IntegerField,SubmitField)
+
+class AddForm(FlaskForm):
+
+    name = StringField('Name of the Puppy : ')
+    submit = SubmitField('Add Puppy')
+
+
+
+class DelForm(FlaskForm):
+    id = IntegerField('Id number of Puppy to Remove : ')
+    submit = SubmitField('Remove Puppy')
